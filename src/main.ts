390a1fa6da8b8a0e4683494c997c0fb6d7eaab5c
AndreyUA <infernalsteel666@gmail.com>
2025-01-04 19:40:10 +0200

    basic setup of three.js project

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,17 @@ import "./style.css";
 
 import * as THREE from "three";
 
-console.log("THREE >> ", THREE);
+const canvas = document.querySelector("canvas.webgl") as HTMLCanvasElement;
+
+const width = globalThis.innerWidth;
+const height = globalThis.innerHeight;
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 100);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+});
+renderer.setSize(width, height);
+renderer.render(scene, camera);
