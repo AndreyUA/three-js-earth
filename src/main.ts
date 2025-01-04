a393e084e01b29264abe9b1a110a8a2ac5f6fbbc
AndreyUA <infernalsteel666@gmail.com>
2025-01-04 20:13:53 +0200

    added animation function

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import "./style.css";
 
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 const canvas = document.querySelector("canvas.webgl") as HTMLCanvasElement;
 
@@ -9,10 +10,23 @@ const height = globalThis.innerHeight;
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 100);
+const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1_000);
+camera.position.z = 5;
+scene.add(camera);
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 const renderer = new THREE.WebGLRenderer({
   canvas,
 });
 renderer.setSize(width, height);
 renderer.render(scene, camera);
+
+const tick = () => {
+  renderer.render(scene, camera);
+
+  globalThis.requestAnimationFrame(tick);
+};
+
+tick();
